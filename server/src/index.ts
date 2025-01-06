1b7933c60803727ebe3ec7937afb208207144f08
ashwink02 <ashwinkotiyal07@gmail.com>
2025-01-06 17:09:27 +0530

    dc4

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://rev-space.vercel.app"],
     credentials: true,
   })
 );
