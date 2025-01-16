8ffcd11c62ce9c1b01645bd6b6c6e56b73431169
ashwink02 <ashwinkotiyal07@gmail.com>
2025-01-16 13:51:54 +0530

    cors dep

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://rev-space.vercel.app"],
+    origin: ["http://localhost:5173", "https://revspace.woksh.com"],
     credentials: true,
   })
 );
